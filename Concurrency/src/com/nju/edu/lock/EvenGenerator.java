d45be4415ef69178eb66a1d0287c093281bd964d
unknown <1754350460@qq.com>
2021-11-04 10:18:55 +0800

    add fundamental lock

--- Concurrency/src/com/nju/edu/lock/EvenGenerator.java
+++ Concurrency/src/com/nju/edu/lock/EvenGenerator.java
@@ -17,6 +17,7 @@ abstract class IntGenerator {
 public class EvenGenerator extends IntGenerator {
     private int currentEvenValue = 0;
 
+    @Override
     public int next() {
         // 这里会出现资源共享问题
         // 具体表现在某个线程在执行到第一句++的时候，另一个线程开始执行，就会导致值不再是偶数
