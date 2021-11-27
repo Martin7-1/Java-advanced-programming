e2abe458a8dd5cd41046576082e4342e2b9562a4
unknown <1754350460@qq.com>
2021-11-27 19:50:24 +0800

    update

--- Net/src/com/nju/edu/net/SimpleSocketClient.java
+++ Net/src/com/nju/edu/net/SimpleSocketClient.java
@@ -1,5 +1,8 @@
 package com.nju.edu.net;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
 
 public class SimpleSocketClient {
@@ -16,7 +19,22 @@ public class SimpleSocketClient {
         System.out.println("Loading contents of URL: " + server);
 
         try {
+            // connect to the server
             Socket socket = new Socket(server, 80);
+
+            // create input and output streams to read from and write to the server
+            PrintStream out = new PrintStream(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // Follow data from the server until we finish reading the document
+            String readLine;
+            while ((readLine = in.readLine()) != null) {
+                System.out.println(readLine);
+            }
+
+            in.close();
+            out.close();
+            socket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
