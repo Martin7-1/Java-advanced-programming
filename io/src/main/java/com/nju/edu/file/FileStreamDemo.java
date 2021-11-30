21127dad95f7c9f2cb6268f8ae1f36d5d406e395
unknown <1754350460@qq.com>
2021-11-30 20:39:22 +0800

    update file stream

---
+++ io/src/main/java/com/nju/edu/file/FileStreamDemo.java
@@ -0,0 +1,20 @@
+package com.nju.edu.file;
+
+public class FileStreamDemo {
+
+    private static final String FILEPATH = "temp.log";
+
+    public static void main(String[] args) throws Exception {
+        write(FILEPATH);
+        read(FILEPATH);
+    }
+
+    public static void write(String filepath) throws Exception {
+
+    }
+
+    public static void read(String filepath) throws Exception {
+        
+    }
+    
+}
